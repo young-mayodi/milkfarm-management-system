0e6f1c2b86a693e7288fd137abb032659000570d
Farm Management System <admin@farmmanagement.com>
2026-01-28 18:53:23 +0300

    Configure Bugsnag to use environment variables

---
+++ config/initializers/bugsnag.rb
@@ -0,0 +1,5 @@
+Bugsnag.configure do |config|
+  config.api_key = ENV['BUGSNAG_API_KEY'] || "2672ee0b55d434f8f910b27eceebca73"
+  config.notify_release_stages = %w[production staging]
+  config.release_stage = ENV['RAILS_ENV'] || 'development'
+end
